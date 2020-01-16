8766a0d4c7bb40938b4bba2fda8c1c8be4d5fa97
Dorin Ciora <dorin.ciora@fortech.ro>
2020-01-16 17:48:30 +0200

    edit

--- src/Modules/Components/Tour/Tour.js
+++ src/Modules/Components/Tour/Tour.js
@@ -1,35 +1,37 @@
-var lat = [];
-var alt = [];
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(extractPosition);
+$(window).load(function() {
+    if ('geolocation' in navigator) {
+        //check geolocation available
+        //try to get user current location using getCurrentPosition() method
+        navigator.geolocation.getCurrentPosition(function(position) {
+            searchID(position.coords);
+        });
     } else {
-        x.innerHTML = 'Geolocation is not supported by this browser.';
+        alert("Browser doesn't support geolocation!");
     }
-}
-
-function extractPosition(position) {
-    var a = position.coords.latitude;
-    var b = position.coords.longitude;
-}
-
-$.ajax({
-    url:
-        'https://www.outdooractive.com/api/project/api-dev-oa/nearby/tour/jsonp?location=13.404954,52.52000659999999&radius=100000',
-    data: data,
-    success: success,
-    dataType: dataType
 });
 
-function getTours() {
+searchID = coords => {
     fetch(
-        `https://www.outdooractive.com/api/project/api-dev-oa/nearby/tour/jsonp?location=${extractPosition}.${52000659999999}&radius=100000`
+        `https://www.outdooractive.com/api/project/api-dev-oa/nearby/tour/jsonp?location=${coords.latitude},${coords.longitude}&radius=5000000`
     )
-        .then(response => {
-            return response.json();
-        })
-        .then(myJson => {
-            console.log(myJson);
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if (data) {
+                takeID(data.result);
+                console.log(data.result[133]);
+            } else {
+                console.log("can't take data");
+            }
         });
-}
+};
+
+takeID = result => {
+    fetch(
+        `https://www.outdooractive.com/api/oois/jsonp/${result[0].id},${result[1].id},${result[2].id}?key=yourtest-outdoora-ctiveapi&display=list`
+    )
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        });
+};
